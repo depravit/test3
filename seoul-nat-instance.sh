12375ff6752b873d6d6fd4200d3564d5a15ec02d
depravit <157346133+depravit@users.noreply.github.com>
2024-03-29 14:41:35 +0900

    Add files via upload

--- seoul-nat-instance.sh
+++ seoul-nat-instance.sh
@@ -4,4 +4,6 @@ sudo sysctl  -p /etc/sysctl.conf
 sudo yum install -y iptables-services
 sudo systemctl start iptables.service
 sudo systemctl enable iptables.service
+sudo iptables -F
 sudo iptable -t nat -A POSTROUTING -o eth0 -j MASQUERADE
+sudo service iptables save
